0c81833d5c9b4f07321b315f8ed6ee8e8dd2797b
leejh8997 <chelee24@naver.com>
2025-05-28 17:12:04 +0900

    2025.05.28 내장 DB

--- lib/day3/ToDoList.dart
+++ lib/day3/ToDoList.dart
@@ -8,7 +8,7 @@ class ToDoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ToDoManager(),
+        home : ToDoManager()
     );
   }
 }
@@ -27,70 +27,97 @@ class _ToDoManagerState extends State<ToDoManager> {
   void addToDo(){
     String title = txtCtrl.text;
     setState(() {
-      list.add({"title": title, "isDone": false});
+      list.add({"title" : title, "isDone" : false});
       txtCtrl.clear();
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("할 일"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                  child: TextField(
-                    controller: txtCtrl,
-                    decoration:  InputDecoration(
-                      hintText: "할 일 입력 하세요."
-                    ),
-                  ),
-              ),
-              ElevatedButton(
-                  onPressed: (){
-                    addToDo();
-                  },
-                  child: Text("추가")
-              )
-            ],
+  void deleteTodo(int index){
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title : Text("삭제"),
+        content: Text("정말 삭제 하실?"),
+        actions: [
+          TextButton(
+              onPressed: (){
+                setState(() {
+                  list.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text("삭제")
           ),
-          Expanded(
-              child: list.isEmpty
-                ? Center(
-                child: Text("할 일이 없습니다."),
-              )
-                : ListView.builder(
-                  itemCount: list.length,
-                  itemBuilder: (context, index){
-                    Map item = list[index];
-                    return ListTile(
-                      title: Text(
-                        item["title"],
-                        style: TextStyle(
-                          decoration: item["isDone"] ? TextDecoration.lineThrough : null
-                        ),
-                      ),
-                      leading: Checkbox(
-                          value: item["isDone"],
-                          onChanged: (value){
-                            setState(() {
-                              item["isDone"] = value;
-                            });
-                          }),
-                      trailing: IconButton(
-                          onPressed: (){} ,
-                          icon: Icon(Icons.delete)),
-                    );
-                  })
+          TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: Text("취소")
           )
         ],
       ),
     );
   }
-}
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title : Text("할 일"),
+          centerTitle: true,
+        ),
+        body : Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    child: TextField(
+                      controller: txtCtrl,
+                      decoration: InputDecoration(
+                          hintText: "할 일 입력 하셔"
+                      ),
+                    )
+                ),
+                SizedBox(width: 20,),
+                ElevatedButton(
+                    onPressed: addToDo,
+                    child: Text("추가")
+                )
+              ],
+            ),
+            Expanded(
+                child: list.isEmpty
+                    ? Center(child: Text("할 일 없음"),)
+                    : ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (context, index) {
+                    Map item = list[index];
+                    return ListTile(
+                      title : Text(
+                          item["title"],
+                          style : TextStyle(
+                            decoration: item["isDone"] ? TextDecoration.lineThrough : null,
+                          )
+                      ),
+                      leading: Checkbox(
+                        value: item["isDone"],
+                        onChanged: (value) {
+                          setState(() {
+                            item["isDone"] = value;
+                          });
+                        },
+                      ),
+                      trailing: IconButton(
+                          onPressed: () => deleteTodo(index),
+                          icon: Icon(Icons.delete)
+                      ),
+                    );
+                  },
+                )
+
+            )
+          ],
+        )
+    );
+  }
+}
---
+++ lib/day4/db_sample.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import '/common/db.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SQLite 실습',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DBWidget(),
+    );
+  }
+}
+
+class DBWidget extends StatefulWidget {
+  @override
+  _DBWidgetState createState() => _DBWidgetState();
+}
+
+class _DBWidgetState extends State<DBWidget> {
+  List<Map<String, dynamic>> item = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();  // 데이터 조회 함수 호출
+  }
+
+  // 데이터베이스에서 데이터 조회
+  Future<void> _fetchData() async {
+    List<Map<String, dynamic>> data = await DB.getData();
+    setState(() {
+      item = data;  // 조회한 데이터를 리스트에 저장
+    });
+  }
+
+  void editItem(int id, String name, int value){
+    TextEditingController nameCtrl = TextEditingController(text : name);
+    TextEditingController valueCtrl = TextEditingController(text : value.toString());
+
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("수정"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: nameCtrl,
+                  decoration: InputDecoration(labelText: "이름"),
+                ),
+                TextField(
+                  controller: valueCtrl,
+                  decoration: InputDecoration(labelText: "값"),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () async {
+                    String editName = nameCtrl.text;
+                    int editValue = int.tryParse(valueCtrl.text) ?? 0;
+                    await DB.updateData(id, editName, editValue);
+                    Navigator.of(context).pop();
+                    _fetchData();
+                  }, 
+                  child: Text("저장")
+              ),
+              TextButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  }, 
+                  child: Text("취소")
+              )
+            ],
+          );
+        },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SQLite 예제'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // 데이터를 출력할 ListView
+            Expanded(
+              child: ListView.builder(
+                itemCount: item.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(item[index]['name']),
+                    subtitle: Text('Value: ${item[index]['value']}, Num: ${item[index]['num']}'),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                            onPressed: (){
+                              editItem(
+                                  item[index]['id'],
+                                  item[index]['name'],
+                                  item[index]['value']
+                              );
+                            },
+                            icon: Icon(Icons.edit)
+                        ),
+                        IconButton(
+                            onPressed: () async {
+                              int id = item[index]["id"];
+                              await DB.deleteData(id);
+                              _fetchData();
+                            },
+                            icon: Icon(Icons.delete)
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            // 새 데이터 추가 버튼
+            ElevatedButton(
+              onPressed: () async {
+                await DB.insertData('추가된 아이템', 100, 3);
+                _fetchData();  // 데이터 새로 고침
+              },
+              child: Text('추가'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
